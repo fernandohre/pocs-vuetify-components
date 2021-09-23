c5115e026d86fd89fd61123e7f2e98b9ead0efb8
Fernando Callata <fernando.henrique@hiper.com.br>
2021-09-23 00:59:57 -0300

    testando v-list

---
+++ app/src/components/shared/Painel.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-flex">
+    <div class="col-md-4">
+      <v-list shaped>
+        <v-list-item v-for="n in 5" :key="n" link>
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </div>
+    <div class="col-md-8">
+        <v-container> Olá </v-container>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  props: [],
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -3,30 +3,41 @@ import VueRouter from 'vue-router'
 import Inicio from '@/components/Inicio.vue'
 import Cliente from '@/components/Cliente.vue'
 import Contador from '@/components/Contador.vue'
+import Painel from '@/components/shared/Painel.vue'
 Vue.use(VueRouter)
 
 const routes = [
-    {
-      path: '/',
-      name: 'Inicio',
-      component: Inicio
-    },
-    {
-        path: '/Cliente',
-        name: 'Cliente',
-        component: Cliente
-    },
-    {
-        path: '/Contador',
-        name: 'Contador',
-        component: Contador
-    }
-  ]
-  
-  const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
-  })
-  
-  export default router
+  {
+    path: '/',
+    name: 'Inicio',
+    component: Inicio
+  },
+  {
+    path: '/Cliente',
+    name: 'Cliente',
+    component: Cliente
+  },
+  {
+    path: '/Contador',
+    name: 'Contador',
+    component: Contador
+  },
+  {
+    path: '/Contador/Painel',
+    name: 'Painel',
+    component: Painel
+  },
+  {
+    path: '/Cliente/Painel',
+    name: 'Painel',
+    component: Painel
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
